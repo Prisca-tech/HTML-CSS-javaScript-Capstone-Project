339f46fd426ff9e511093225085a9a1c7a01d718
Priscilla Okoro <oyomepriscilla@gmail.com>
2022-06-30 03:22:24 -0600

    add linters file, fix linters errors

--- script.js
+++ script.js
@@ -1,48 +1,48 @@
 // Display teahers section dynamically
 const myArray = [
-    {
-        //  Teacher 1
-       image:'./Assets/career.jpg',
-       teacherName: 'Eva Alordia',
-       designation: 'Career Coach',
-       text: 'Lorem, ipsum dolor sit amet consectetur adipisicing',
-    },
-    // Teacher 2
-    {
-        image:'./Assets/Ben.jpg',
-        teacherName: 'Benedict Simon',
-        designation: 'Backend Dev.',
-        text: 'Lorem, ipsum dolor sit amet consectetur adipisicing',
-    },
- //  Teacher 3
-    {
-        image:'./Assets/Precious.jpg',
-        teacherName: 'Precious Udegbue',
-        designation: 'Front-end Dev.',
-        text: 'Lorem, ipsum dolor sit amet consectetur adipisicing',
-    },
- //  Teacher 4
-    {
-        image:'./Assets/killy.jpg',
-        teacherName: 'Kingsley Okine',
-        designation: 'Design/Tech Support',
-        text: 'Lorem, ipsum dolor sit amet consectetur adipisicing',
-    },
- //  Teacher 5
-    {
-        image:'./Assets/bem.webp',
-        teacherName: 'Abe Precious',
-        designation: 'Student Success',
-        text: 'Lorem, ipsum dolor sit amet consectetur adipisicing',
-    },
- //  Teacher 6
-    {
-        image:'./Assets/Administrator.jpg',
-        teacherName: 'Godfrey Grace',
-        designation: 'Administrator',
-        text: 'Lorem, ipsum dolor sit amet consectetur adipisicing',
-    }
-]
+  {
+    //  Teacher 1
+    image: './Assets/career.jpg',
+    teacherName: 'Eva Alordia',
+    designation: 'Career Coach',
+    text: 'Lorem, ipsum dolor sit amet consectetur adipisicing',
+  },
+  // Teacher 2
+  {
+    image: './Assets/Ben.jpg',
+    teacherName: 'Benedict Simon',
+    designation: 'Backend Dev.',
+    text: 'Lorem, ipsum dolor sit amet consectetur adipisicing',
+  },
+  //  Teacher 3
+  {
+    image: './Assets/Precious.jpg',
+    teacherName: 'Precious Udegbue',
+    designation: 'Front-end Dev.',
+    text: 'Lorem, ipsum dolor sit amet consectetur adipisicing',
+  },
+  //  Teacher 4
+  {
+    image: './Assets/killy.jpg',
+    teacherName: 'Kingsley Okine',
+    designation: 'Design/Tech Support',
+    text: 'Lorem, ipsum dolor sit amet consectetur adipisicing',
+  },
+  //  Teacher 5
+  {
+    image: './Assets/bem.webp',
+    teacherName: 'Abe Precious',
+    designation: 'Student Success',
+    text: 'Lorem, ipsum dolor sit amet consectetur adipisicing',
+  },
+  //  Teacher 6
+  {
+    image: './Assets/Administrator.jpg',
+    teacherName: 'Godfrey Grace',
+    designation: 'Administrator',
+    text: 'Lorem, ipsum dolor sit amet consectetur adipisicing',
+  },
+];
 const cardContainer = document.getElementById('facilitators-persons');
 
 const cards = myArray.map((card) => ` <div class="facilitators-info">
@@ -61,9 +61,8 @@ cardContainer.innerHTML += cards;
 
 // Display mobile menu
 const hamburger = document.getElementById('hamburger');
-const menu = document.getElementById('desktop-nav')
+const menu = document.getElementById('desktop-nav');
 const menuItems = document.querySelector('.close-menu');
-const logo = document.querySelector('#heading-text')
 
 function openMenu() {
   hamburger.classList.toggle('fa-xmark');
